029197ab95e5ec93c1a919849f291d36b03f1393
Vladislav Fateev <Fateev_wbd@mail.ru>
2023-11-01 22:05:53 +0500

    add filter

---
+++ js/script.js
@@ -0,0 +1,35 @@
+/*-------------Burger-------------*/
+
+const burger = document.querySelector('.menu-icon')
+const menu = document.querySelector('.menu')
+const body = document.body
+
+if (burger && menu) {
+    burger.addEventListener('click', ()=>{
+        burger.classList.toggle('_active')
+        menu.classList.toggle('_active')
+        body.classList.toggle('_lock')
+
+    })
+}
+/*-------------/Burger-------------*/
+
+/*-------------Filter-------------*/
+const filter = document.querySelector('.filter')
+
+if(filter) {
+    const items = filter.querySelectorAll('.block-filter')
+
+    items.forEach(item => {
+        item.addEventListener('click', event => {
+            item.querySelector('.block-filter__dropdown').classList.toggle('_active')
+            item.querySelector('.block-filter__icon').classList.toggle('_active')
+
+            if(event.target.classList.contains('block-filter__item')) { //эта часть выводит в фильтр выбранное наечение
+                item.querySelector('.block-filter__value').textContent = event.target.textContent
+            }
+        })
+    });
+}
+
+/*-------------/Filter-------------*/
